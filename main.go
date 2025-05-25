6ae8c8e56da935c37945e8bbbc2d4b16ff52e07f
oxkekd <bravl2010@bk.ru>
2025-05-25 18:16:04 +0300

    add telegramm bot api

--- main.go
+++ main.go
@@ -7,13 +7,50 @@ import (
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/mem"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func main() {
+func getServerStats() []float64 {
 	percent, _ := cpu.Percent(time.Second, false)
 	mem, _ := mem.VirtualMemory()
 	disk, _ := disk.Usage("/")
-	fmt.Println(float64(disk.Free) / 1024 / 1024 / 1024)
-	fmt.Println(mem.UsedPercent)
-	fmt.Println(percent[0])
+	stats := []float64{
+		percent[0],
+		mem.UsedPercent,
+		float64(disk.Free) / 1024 / 1024 / 1024,
+	}
+	return stats
+}
+func main() {
+	bot, err := tgbotapi.NewBotAPI("7473696296:AAEDG99W35oMBUV9htfP2P7ksd9Fwft28cc")
+	if err != nil {
+		fmt.Println("ошибка получения newbotapi:", err)
+	}
+
+	bot.Debug = true
+
+	updconf := tgbotapi.NewUpdate(0)
+	updconf.Timeout = 60
+
+	updates := bot.GetUpdatesChan(updconf)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.Text == "stats" {
+			if err != nil {
+				fmt.Println("ошибка получения статов: ", err)
+			}
+			sstats := getServerStats()
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("🔥 CPU: %.1f%%\n🧩 MEM: %.1f%%\n💾 DISK: %.1fGB\n", sstats[0], sstats[1], sstats[2]))
+			bot.Send(msg)
+		}
+	}
+	/*
+		fmt.Println(float64(disk.Free) / 1024 / 1024 / 1024)
+		fmt.Println(mem.UsedPercent)
+		fmt.Println(percent[0])
+	*/
 }
