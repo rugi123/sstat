f8343320de67b5592192c24ff669888747f71533
oxkekd <bravl2010@bk.ru>
2025-05-25 17:34:06 +0300

    delete gopsutil sensors

--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/shirou/gopsutil/v3/cpu"
-        "github.com/shirou/gopsutil/v3/mem"
-        "github.com/shirou/gopsutil/v3/disk"
-        "github.com/shirou/gopsutil/v3/sensors"
-        "time"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func main() {
-    percent, _ := cpu.Percent(time.Second, false)
-    mem, _ := mem.VirtualMemory()
-    disk, _ := disk.Usage("/")
-    temp, _ := sensors.SensorsTemperatures()
-    fmt.Println(temp[0])
-    fmt.Println(float64(disk.Free)/1024/1024/1024)
-    fmt.Println(mem.UsedPercent)
-    fmt.Println(percent[0])
+	percent, _ := cpu.Percent(time.Second, false)
+	mem, _ := mem.VirtualMemory()
+	disk, _ := disk.Usage("/")
+	fmt.Println(float64(disk.Free) / 1024 / 1024 / 1024)
+	fmt.Println(mem.UsedPercent)
+	fmt.Println(percent[0])
 }
